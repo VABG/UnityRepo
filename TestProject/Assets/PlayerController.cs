9d10c8fc2cd3196b55a531691eb30e2136f6f3c8
VABG <VABGProductions@hotmail.com>
2021-03-30 16:05:20 +0200

    Raycast not working!!! GRR!

---
+++ TestProject/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public bool MoveTo(Vector3 pos)
+    {
+        return navMeshAgent.SetDestination(pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
